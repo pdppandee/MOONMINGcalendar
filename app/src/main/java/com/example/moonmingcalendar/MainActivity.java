401b7f9bbe2633feca6ae1f64d7295fb564d9fa4
mymayy <metawee_2544@hotmail.com>
2021-11-06 21:33:26 +0700

    show date event list

--- app/src/main/java/com/example/moonmingcalendar/MainActivity.java
+++ app/src/main/java/com/example/moonmingcalendar/MainActivity.java
@@ -4,15 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,11 +44,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
-
-
-
-
 
 }
--- app/src/main/java/com/example/moonmingcalendar/ShowEventList.java
+++ app/src/main/java/com/example/moonmingcalendar/ShowEventList.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,13 +13,15 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class ShowEventList extends AppCompatActivity{
     Button btnBack,btnMutelu;
     FloatingActionButton fabAdd;
+    TextView textDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_event_list);
-
+        textDate = findViewById(R.id.date);
         String day = getIntent().getExtras().getString("day");
+        textDate.setText(day);
         btnMutelu = findViewById(R.id.mutelu);
         btnMutelu.setOnClickListener(new View.OnClickListener() {
             @Override

753da40582275b7f27f40361f0effcdfb5cd4c23
varisara2103 <11241varisara@gmail.com>
2021-11-07 16:08:31 +0700

    แก้ showUserEvent เพิ่ม EditUserEvent

--- app/src/main/java/com/example/moonmingcalendar/EditUserEvent.java
+++ app/src/main/java/com/example/moonmingcalendar/EditUserEvent.java
@@ -1,4 +1,46 @@
 package com.example.moonmingcalendar;
 
-public class EditUserEvent {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditUserEvent extends AppCompatActivity {
+    Button btnSaveEditEvent,btnBackToShowUE;
+    TextView textEditEventName,textEditDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_user_event);
+        textEditDate = findViewById(R.id.editueDate);
+        String day = getIntent().getExtras().getString("day");
+        textEditDate.setText(day);
+        textEditEventName = findViewById(R.id.editNameEvent);
+
+        btnSaveEditEvent = findViewById(R.id.editueSave);
+        btnSaveEditEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent saveEditEvent = new Intent(EditUserEvent.this, ShowEventList.class);
+                Toast.makeText(EditUserEvent.this, "แก้ไขเสร็จสิ้นจ้า :-D", Toast.LENGTH_SHORT).show();
+                startActivity(saveEditEvent);
+            }
+        });
+
+        btnBackToShowUE = findViewById(R.id.editueBack);
+        btnBackToShowUE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backToShowUserEvent = new Intent(EditUserEvent.this, ShowEventList.class);
+                startActivity(backToShowUserEvent);
+            }
+        });
+
+
+    }
 }
--- app/src/main/java/com/example/moonmingcalendar/ShowUserEvent.java
+++ app/src/main/java/com/example/moonmingcalendar/ShowUserEvent.java
@@ -9,40 +9,45 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShowUserEvent extends AppCompatActivity {
-    Button btnUlBack,btnUlEdit,btnUldelete;
+    Button btnUlBack,btnUlEdit,btnUlDelete;
     TextView textEventName,textDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_user_event);
-        textDate = findViewById(R.id.ulDate);
+        textDate = findViewById(R.id.ueDate);
         String day = getIntent().getExtras().getString("day");
         textDate.setText(day);
-
         textEventName = findViewById(R.id.eventName);
         //textEventName.setText(eventName);
-
-        btnUlBack = findViewById(R.id.ulBack);
+        btnUlBack = findViewById(R.id.ueBack);
         btnUlBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent1 = new Intent(getApplicationContext(), ShowEventList.class);
-                startActivity(intent1);
+                Intent backToShowEventList = new Intent(ShowUserEvent.this, ShowEventList.class);
+                startActivity(backToShowEventList);
             }
         });
 
-        btnUlEdit = findViewById(R.id.ulEdit);
+        btnUlEdit = findViewById(R.id.ueEdit);
         btnUlEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent2 = new Intent(ShowUserEvent.this, EditUserEvent.class);
-                intent2.putExtra("day", day);
-                startActivity(intent2);
+                Intent editUserEvent = new Intent(ShowUserEvent.this, EditUserEvent.class);
+                editUserEvent.putExtra("day", day);
+                startActivity(editUserEvent);
             }
         });
 
-        btnUldelete = findViewById(R.id.ulDelete);
+        btnUlDelete = findViewById(R.id.ueDelete);
+        /**btnUlDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 = new Intent(ShowUserEvent.this, ShowEventList.class);
+                startActivity(intent3);
+            }
+        });**/
     }
 }
 

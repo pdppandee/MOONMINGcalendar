5f1ac472ff61e06ab79238c5084df5f311a8a8f2
thispan <panda131243@gmail.com>
2021-11-11 00:16:53 +0700

    edit db but not success + add user event class

--- app/src/main/java/com/example/moonmingcalendar/AddUserEvent.java
+++ app/src/main/java/com/example/moonmingcalendar/AddUserEvent.java
@@ -1,12 +1,18 @@
 package com.example.moonmingcalendar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddUserEvent extends AppCompatActivity {
     EditText eventName, eventDetail;
+    Button addUserEventButton;
+    ListView userEventListView;
 
     DBmoonming databaseHelper;
 
@@ -18,6 +24,30 @@ public class AddUserEvent extends AppCompatActivity {
 
             eventName = findViewById(R.id.eventname);
             eventDetail = findViewById(R.id.eventDetail);
+            userEventListView = findViewById(R.id.userEventListView);
+            addUserEventButton = findViewById(R.id.addUserEventdbutton);
+
+            addUserEventButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    UserEvent event1;
+                    try {
+                         event1 = new UserEvent(eventName.getText().toString(), eventDetail.getText().toString());
+                        Toast.makeText(getApplicationContext(),event1.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                    catch (Exception e){
+                        event1 = new UserEvent("error", "error");
+                    }
+//                    UserEvent event1 = new UserEvent(eventName.getText().toString(), eventDetail.getText().toString());
+
+
+                    databaseHelper = new DBmoonming(getApplicationContext());
+
+                    boolean success = databaseHelper.addUserEvent(event1);
+
+                    Toast.makeText(getApplicationContext(), "Success " + success, Toast.LENGTH_SHORT).show();
+                }
+            });
     }
 
 }
---
+++ app/src/main/java/com/example/moonmingcalendar/UserEvent.java
@@ -0,0 +1,44 @@
+package com.example.moonmingcalendar;
+
+public class UserEvent {
+    private String datetime;
+    private String eventName;
+    private String eventDetail;
+
+    public UserEvent (String eventName, String eventDetail){
+        this.eventDetail = eventDetail;
+        this.eventName = eventName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEvent{" +
+                "eventName='" + eventName + '\'' +
+                ", eventDetail='" + eventDetail + '\'' +
+                '}';
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventDetail() {
+        return eventDetail;
+    }
+
+    public void setEventDetail(String eventDetail) {
+        this.eventDetail = eventDetail;
+    }
+}

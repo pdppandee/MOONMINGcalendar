e483186f57126ad8bb6f896dfb4645526f850279
mymayy <metawee_2544@hotmail.com>
2021-11-11 22:37:49 +0700

    edit showevent

--- app/src/main/java/com/example/moonmingcalendar/MainActivity.java
+++ app/src/main/java/com/example/moonmingcalendar/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -41,16 +42,6 @@ public class MainActivity extends AppCompatActivity {
                 ArrayList<String> eventListName=ShowEvent.getEventListName();
                 ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, eventListName);
                 miniEventList.setAdapter(arrayAdapter);
-
-                /*if(dayOfMonth == 10) {
-                    ArrayList<String> arrayList = new ArrayList<>();
-                    arrayList.add("Moonming Diagram");
-                    arrayList.add("SE advSE Clip");
-                    arrayList.add("Apple Music");
-
-                    ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
-                    miniEventList.setAdapter(arrayAdapter);
-                }*/
 
                 btnDetail = findViewById(R.id.btnDetail);
                 btnDetail.setOnClickListener(new View.OnClickListener() {

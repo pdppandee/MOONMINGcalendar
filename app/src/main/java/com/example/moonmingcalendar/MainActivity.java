96fce760a8482c95eb30ad64775e88ccd2829189
mprngpkt <mp.mppkt@gmail.com>
2021-11-11 20:35:49 +0700

    add small list below big calendar but not 100%

--- app/src/main/java/com/example/moonmingcalendar/MainActivity.java
+++ app/src/main/java/com/example/moonmingcalendar/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     CalendarView mCalendar;
     Button btnDetail;
     TextView textDate;
-
+    ListView miniEventList;
 
 
     @Override
@@ -28,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         textDate = findViewById(R.id.Date);
         mCalendar = findViewById(R.id.calendarView);
 
+        miniEventList = findViewById(R.id.eventList);
+
         mCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
@@ -36,6 +40,15 @@ public class MainActivity extends AppCompatActivity {
                 GetMainEvent ShowEvent = new GetMainEvent(day);
                 String showEvent = ShowEvent.getMainActivity();
 
+                if(dayOfMonth == 10) {
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    arrayList.add("Moonming Diagram");
+                    arrayList.add("SE advSE Clip");
+                    arrayList.add("Apple Music");
+
+                    ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, arrayList);
+                    miniEventList.setAdapter(arrayAdapter);
+                }
 
                 btnDetail = findViewById(R.id.btnDetail);
                 btnDetail.setOnClickListener(new View.OnClickListener() {

6af3746f8979be34524ea3d694fccf9f59cd0786
mprngpkt <mp.mppkt@gmail.com>
2021-11-13 22:00:46 +0700

    byebye date picker

--- app/src/main/java/com/example/moonmingcalendar/AddUserEvent.java
+++ app/src/main/java/com/example/moonmingcalendar/AddUserEvent.java
@@ -1,19 +1,23 @@
 package com.example.moonmingcalendar;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class AddUserEvent extends AppCompatActivity {
     EditText eventName, eventDetail;
     Button addUserEventButton;
     ListView userEventListView;
-
     DBmoonming databaseHelper;
 
     @Override
@@ -22,36 +26,31 @@ public class AddUserEvent extends AppCompatActivity {
         setContentView(R.layout.add_user_event);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-            eventName = findViewById(R.id.eventname);
-            eventDetail = findViewById(R.id.eventDetail);
-            userEventListView = findViewById(R.id.userEventListView);
-            addUserEventButton = findViewById(R.id.addUserEventdbutton);
+        eventName = findViewById(R.id.eventname);
+        eventDetail = findViewById(R.id.eventDetail);
+        userEventListView = findViewById(R.id.userEventListView);
+        addUserEventButton = findViewById(R.id.addUserEventButton);
 
-            addUserEventButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    UserEvent event1;
-                    try {
-                         event1 = new UserEvent(eventName.getText().toString(), eventDetail.getText().toString());
-                        Toast.makeText(getApplicationContext(),event1.toString(), Toast.LENGTH_SHORT).show();
-                    }
-                    catch (Exception e){
-                        event1 = new UserEvent("error", "error");
-                    }
+        addUserEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserEvent event1;
+                try {
+                    event1 = new UserEvent(eventName.getText().toString(), eventDetail.getText().toString());
+                    Toast.makeText(getApplicationContext(), event1.toString(), Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    event1 = new UserEvent("error", "error");
+                }
 //                    UserEvent event1 = new UserEvent(eventName.getText().toString(), eventDetail.getText().toString());
 
 
-                    databaseHelper = new DBmoonming(getApplicationContext());
+                databaseHelper = new DBmoonming(getApplicationContext());
 
-                    boolean success = databaseHelper.addUserEvent(event1);
+                boolean success = databaseHelper.addUserEvent(event1);
 
-                    Toast.makeText(getApplicationContext(), "Success " + success, Toast.LENGTH_SHORT).show();
-                }
-            });
-    }
-
-    public void openDatePicker(View View){
-
+                Toast.makeText(getApplicationContext(), "Success " + success, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }

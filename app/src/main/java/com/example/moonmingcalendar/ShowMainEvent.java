9a2ecd026a709094d46405543e068dfdc3f244de
mymayy <metawee_2544@hotmail.com>
2021-11-14 21:36:45 +0700

    edit show main event

--- app/src/main/java/com/example/moonmingcalendar/ShowMainEvent.java
+++ app/src/main/java/com/example/moonmingcalendar/ShowMainEvent.java
@@ -9,14 +9,16 @@ import java.util.ArrayList;
 
 public class ShowMainEvent extends AppCompatActivity {
 
-    TextView textMainEventName, textMainEventDate, test;
+    TextView textMainEventName, textMainEventDate, textMainEventDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.show_main_event);
         textMainEventName = findViewById(R.id.maineventname);
+        textMainEventDetail = findViewById(R.id.maineventdetail);
         textMainEventDate = findViewById(R.id.date);
+
         String day = getIntent().getExtras().getString("day");
         int position = getIntent().getExtras().getInt("position");
         GetMainEvent showEvent = new GetMainEvent(day);
@@ -24,9 +26,8 @@ public class ShowMainEvent extends AppCompatActivity {
         String eventName = showEvent.getEventListName().get(position);
         String eventDetail = showEvent.getEventListDetail().get(position);
 
-        test=findViewById(R.id.test);
-        test.setText(eventName+day+eventDetail);
-
+        textMainEventName.setText(eventName);
+        textMainEventDetail.setText(eventDetail);
         //textMainEventDate.setText(day);
     }
 }

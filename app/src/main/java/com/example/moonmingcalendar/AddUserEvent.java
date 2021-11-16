022124066d7a1b7710aa4293f9c55e37c07e5982
thispan <panda131243@gmail.com>
2021-11-16 13:37:26 +0700

    noti in add user but not success yet

--- app/src/main/java/com/example/moonmingcalendar/AddUserEvent.java
+++ app/src/main/java/com/example/moonmingcalendar/AddUserEvent.java
@@ -1,7 +1,13 @@
 package com.example.moonmingcalendar;
 
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,8 +17,9 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.moonmingcalendar.databinding.ActivityMainBinding;
+import com.example.moonmingcalendar.databinding.AddUserEventBinding;
 
+import java.util.Calendar;
 import java.util.Locale;
 
 public class AddUserEvent extends AppCompatActivity {
@@ -22,7 +29,8 @@ public class AddUserEvent extends AppCompatActivity {
     TimePicker timePicker;
     int hour, minute;
 
-    private ActivityMainBinding binding;
+
+    private AddUserEventBinding binding;
 
 
     @Override
@@ -31,19 +39,21 @@ public class AddUserEvent extends AppCompatActivity {
         setContentView(R.layout.add_user_event);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-//        binding = ActivityMainBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//        createNotificationChannel();
+        binding = AddUserEventBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        createNotificationChannel();
 
-
-
-
+        binding.openNoti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    scheduleNotification();
+            }
+        });
 
         eventName = findViewById(R.id.eventname);
         eventDetail = findViewById(R.id.eventDetail);
         addUserEventButton = findViewById(R.id.addUserEventButton);
-        selectTimeButton = findViewById(R.id.selectTimeBtn);
         pDatabaseHelper = new DbPayHelper(this);
         timePicker = findViewById(R.id.timePicker);
 
@@ -62,7 +72,7 @@ public class AddUserEvent extends AppCompatActivity {
                     AddData(day,name,detail,noti,time);
                     eventName.setText("");
                     eventDetail.setText("");
-                    selectTimeButton.setText("เลือกเวลา");
+//                    selectTimeButton.setText("เลือกเวลา");
                 }else if(eventName.length() != 0 && eventDetail.length() != 0 && time.equals("เลือกเวลา")){
                     toastMessage("กรุณาระบุเวลาด้วยจ้า!");
                 }
@@ -94,7 +104,68 @@ public class AddUserEvent extends AppCompatActivity {
         });
     }
 
+    private void scheduleNotification() {
+        Intent intent = new Intent(getApplicationContext(), Notifications.class);
+        String title = binding.eventname.getText().toString();
+        String content = binding.eventDetail.getText().toString();
+        Notifications noti = new Notifications();
+
+        intent.putExtra(noti.getTitleExtra(), title);
+        intent.putExtra(noti.getContentText() , content);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+          getApplicationContext(), noti.getNotificationID(),intent,
+                PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT
+        );
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Long time = getTime();
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, time, pendingIntent);
+
+        Toast.makeText(getApplicationContext(),"ตั้งแจ้งเตือน", Toast.LENGTH_SHORT).show();
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            alarmManager.setExactAndAllowWhileIdle(
+//                    AlarmManager.RTC_WAKEUP, time, pendingIntent
+//
+//            );
+//        }
+//        showAlert(time, title, content);
+
+    }
+
+//    private void showAlert(Long time, String title, String content) {
+//        Date date = new Date(time);
+//
+//    }
+
+    private Long getTime() {
+        Integer minute = binding.timePicker.getCurrentMinute();
+        Integer hours = binding.timePicker.getCurrentHour();
+        String date = getIntent().getExtras().getString("day");
+        String[] datearr = date.split("/");
+        Integer day = Integer.parseInt(datearr[0]);
+        Integer month = Integer.parseInt(datearr[1]);
+        Integer year = Integer.parseInt(datearr[2]);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day, hours, minute);
+        return calendar.getTimeInMillis();
+    }
+
     private void createNotificationChannel() {
+        Notifications noti = new Notifications();
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "Notif Channel";
+            String desc = "desc";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(noti.getChannelID(),name,importance);
+            channel.setDescription(desc);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+
     }
 
 
--- app/src/main/java/com/example/moonmingcalendar/Notifications.java
+++ app/src/main/java/com/example/moonmingcalendar/Notifications.java
@@ -1,6 +1,5 @@
 package com.example.moonmingcalendar;
 
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,27 +8,50 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 public class Notifications extends BroadcastReceiver {
+
+    Integer notificationID = 1;
+    public String channelID = "channel 1";
+    String titleExtra = "Ayy Yo";
+    String contentText = "งว่วง";
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Intent intentadduserevent = new Intent(context,AddUserEvent.class);
-        intentadduserevent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context,0, intentadduserevent,0);
+//        Intent intentadduserevent = new Intent(context,AddUserEvent.class);
+//        intentadduserevent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(context,0, intentadduserevent,0);
 
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"foxandroid")
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(context,"channelID")
                 .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("AYYY YOO!")
-                .setContentText("eiei")
+                .setContentTitle(intent.getStringExtra(titleExtra))
+                .setContentText(intent.getStringExtra(contentText))
                 .setAutoCancel(true)
                 .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setContentIntent(pendingIntent);
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+//                .setContentIntent(pendingIntent);
 
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
-        notificationManagerCompat.notify(123,builder.build());
+        notificationManagerCompat.notify(notificationID,notification.build());
 
 
+    }
+
+    public String getChannelID(){
+        return channelID;
+    }
+
+    public String getTitleExtra() {
+        return titleExtra;
+    }
+
+    public String getContentText() {
+        return contentText;
+    }
+
+    public Integer getNotificationID() {
+        return notificationID;
     }
 }
